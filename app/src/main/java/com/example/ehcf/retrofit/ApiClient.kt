40081d9d2a18d102855e68886d1e5010ab4166f1
Somenath-mahata <somenathmahata9@gmail.com>
2024-09-11 18:32:45 +0530

    commited by somenath

--- app/src/main/java/com/example/ehcf/retrofit/ApiClient.kt
+++ app/src/main/java/com/example/ehcf/retrofit/ApiClient.kt
@@ -15,6 +15,7 @@ object ApiClient {
     private var retrofit: Retrofit? = null
     private val client: Retrofit?
         get() {
+            //somenath
             val interceptor = HttpLoggingInterceptor()
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
             val apiClient: OkHttpClient = OkHttpClient.Builder()
